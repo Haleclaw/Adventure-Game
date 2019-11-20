0e3fe12081e3ca3ca8efd79b3e1ede48ff234c64
Bram van Ballegooijen <99046330@mydavinci.nl>
2019-11-20 14:20:19 +0100

    Added main menu

---
+++ adventure.js
@@ -0,0 +1,6 @@
+document.getElementById("button1").innerHTML = "Play"
+document.getElementById("button2").innerHTML = "Help"
+document.getElementById("button3").innerHTML = "Credits"
+
+document.getElementById("title").innerHTML = "Blood and Wine"
+document.getElementById("description").innerHTML = "Welkom bij Blood and wine. Dit is een game waar je via belangrijke keuzes de invloed op het verhaal kan hebben "
