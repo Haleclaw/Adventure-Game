09c2d8f6cb98780a70b7d5322741e95ae0c5a042
Bram van Ballegooijen <99046330@mydavinci.nl>
2019-11-26 10:52:52 +0100

    updated menu, inventory,buttons

--- adventure.js
+++ adventure.js
@@ -1,35 +1,72 @@
 
-// Main menu // blood and wine // Main menu
+// Made by Bram van ballegooijen // blood and wine // Made by Bram van ballegooijen //
+// inventory // blood and wine // inventory //
 
+
+var inventory = document.createElement('div');
+document.body.appendChild(inventory);
+inventory.appendChild(document.getElementById("inventoryItem"))
+inventory.setAttribute('id','inventory')
+
+
+// Main menu // blood and wine // Main menu //
+menu()
+
+
+function menu(){
 document.getElementById("button1").innerHTML = "Play"
 document.getElementById("button2").innerHTML = "Help"
 document.getElementById("button3").innerHTML = "Credits"
 
 document.getElementById("title").innerHTML = "Blood and Wine"
-document.getElementById("description").innerHTML = "Welkom bij Blood and wine. Dit is een game waar je via belangrijke keuzes de invloed op het verhaal kan hebben "
+document.getElementById("description").innerHTML = "Welkom bij Blood and wine. Dit is een game waar je via belangrijke keuzes invloed op het verhaal kan hebben "
 
 button1.onclick = play;
 button2.onclick = help;
 button3.onclick = Credits;
+}
 
-// introductie level // blood and wine // introductie level
+// introductie level // blood and wine // introductie level //
 
 function play(){
-  document.getElementById("button1").innerHTML = "Het werkt"
-  document.getElementById("button2").innerHTML = "Het werkt"
-  document.getElementById("button3").innerHTML ='Het werkt'
+  document.getElementById("button1").innerHTML = "je pakt de trap"
+  document.getElementById("button2").innerHTML = "je gaat door de deur"
+  document.getElementById("button3").innerHTML ='je doorzoekt de dungeon'
   document.getElementById("description").innerHTML = "je word wakker in de kerkers van Evemor"
   
   var achtergrond = document.getElementById('game-container')
   achtergrond.style.backgroundImage = " url('img/dungeon.jpg') ";
   
+  document.getElementById("button1").setAttribute('onclick',"keuze()")
+  document.getElementById("button2").setAttribute('onclick',"keuze()")
+  document.getElementById("button3").setAttribute('onclick',"keuze()")
+  
+
 }
+
+// help pagina // blood and wine // help pagina //
 
 function help(){
+  document.getElementById("button1").innerHTML = "Ga terug"
+  document.getElementById("button2").innerHTML = "Ga terug"
+  document.getElementById("button3").innerHTML = "Ga terug"
+  document.getElementById("description").innerHTML = 'Bij blood and wine heb je invloed op het verhaal doordat je keuzes maakt <br> je maakt keuzes door op buttons te klikken'
 
-
+  document.getElementById("button1").setAttribute('onclick',"menu()")
+  document.getElementById("button2").setAttribute('onclick',"menu()")
+  document.getElementById("button3").setAttribute('onclick',"menu()")
 }
 
-function credits(){
+// credits // blood and wine // credits //
+function Credits(){
+  document.getElementById("button1").innerHTML = "Ga terug"
+  document.getElementById("button2").innerHTML = "Ga terug"
+  document.getElementById("button3").innerHTML = "Ga terug"
+
+  document.getElementById("description").innerHTML = 'Deze game is gemaakt door: <br> Bram van ballegooijen '
+
+  document.getElementById("button1").setAttribute('onclick',"menu()")
+  document.getElementById("button2").setAttribute('onclick',"menu()")
+  document.getElementById("button3").setAttribute('onclick',"menu()")
 
 }
