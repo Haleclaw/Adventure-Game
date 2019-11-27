412be3a04d905bc53c7c048d304d6e4483e0a74a
Bram van Ballegooijen <99046330@mydavinci.nl>
2019-11-27 16:00:09 +0100

    fixed key problem

--- adventure.js
+++ adventure.js
@@ -50,12 +50,16 @@ button3.onclick = Credits;
 // RESET // DEAD FUNCTION // RESET // // RESET // DEAD FUNCTION // RESET // // RESET // DEAD FUNCTION // RESET //
 }
 function dead(){
- if (getzwaard == 'ja') {
+ if (getzwaard == 'ja'){ 
  	document.getElementById('zwaard').style.display = 'none'
  	document.getElementById('inventoryItem').style.display = 'none'
- 	    if(getsleutel =='ja'){
+ 	    if (getsleutel =='ja'){
  	    	 document.getElementById('key').style.display = 'none'
  	    }
+ 	               if (getsleutel =='nee'){
+ 	    	          document.getElementById('key').style.display = 'none'
+ 	    }
+
 
 
  	getzwaard = "nee"
