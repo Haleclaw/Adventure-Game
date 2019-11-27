fa464fec224fab8d078f0795501c78da3e98b210
Bram van Ballegooijen <99046330@mydavinci.nl>
2019-11-27 12:21:30 +0100

    added zwaardtext

--- adventure.js
+++ adventure.js
@@ -22,6 +22,8 @@ menu()
 
 
 function menu(){
+console.log("main menu")
+
 document.getElementById("button1").innerHTML = "Play"
 document.getElementById("button2").innerHTML = "Help"
 document.getElementById("button3").innerHTML = "Credits"
@@ -60,9 +62,11 @@ menu()
 // introductie level // blood and wine // introductie level //
 
 function play(){
-  document.getElementById("button1").innerHTML = "je kijkt rond in kisten"
-  document.getElementById("button2").innerHTML = "je gaat door de deur"
-  document.getElementById("button3").innerHTML ='je doorzoekt de dungeon '
+  console.log("level 1 // main kamer")
+  
+  document.getElementById("button1").innerHTML = "je kijkt rond  <br> <b>(je ziet kisten)</br>"
+  document.getElementById("button2").innerHTML = "je gaat door de deur <br> <b>(de deur heeft een slot)</b>"
+  document.getElementById("button3").innerHTML ='je doorzoekt de dungeon <br> <b>(een koude rilling...)</b> '
   document.getElementById("description").innerHTML = "je word wakker in de kerkers van Evemor"
   
   var achtergrond = document.getElementById('game-container')
@@ -87,6 +91,8 @@ function play(){
 // help pagina // blood and wine // help pagina //
 
 function help(){
+  console.log("Help pagina")
+
   document.getElementById("button1").innerHTML = "Ga terug"
   document.getElementById("button2").innerHTML = "Ga terug"
   document.getElementById("button3").innerHTML = "Ga terug"
@@ -99,6 +105,8 @@ function help(){
 
 // credits // blood and wine // credits //
 function Credits(){
+  console.log("Credits pagina")
+
   document.getElementById("button1").innerHTML = "Ga terug"
   document.getElementById("button2").innerHTML = "Ga terug"
   document.getElementById("button3").innerHTML = "Ga terug"
@@ -116,10 +124,12 @@ function Credits(){
 // goblin attack // goblin attack //
 
 function goblin(){
+  console.log("level 1 // de goblin")
+
   if(aanval == 'nee'){
   document.getElementById("description").innerHTML = "een goblin genaamt <b>ARCO DE GENIEPIGE</b> komt achter de boekenkasten vandaan  en steelt al je geld! HIIIHAAAA.... <br>"
   
-  document.getElementById("button1").innerHTML = "ren weg van de Goblin"
+  document.getElementById("button1").innerHTML = "ren weg van de Goblin <br> <b>(je hebt niks om naartoe te rennen)</b>"
   document.getElementById("button2").innerHTML = "Val de goblin aan"
   document.getElementById('button3').style.display =' none'
   document.getElementById('inventoryItem').style.display = 'none'
@@ -139,6 +149,8 @@ function goblin(){
 }
 
 function goblinAanval(){
+	console.log("level 1 // verslagen")
+
 if ( getzwaard == 'ja'){
     document.getElementById("description").innerHTML = "je hebt de goblin verslaan! je hebt je geld terug en een sleutel gekregen"
     document.getElementById('inventoryItem').style.display = 'block'
@@ -174,6 +186,8 @@ if ( getzwaard == 'ja'){
      }
 }
 function goblinterug(){
+    console.log("level 1 // vermoord")
+
 	document.getElementById("description").innerHTML = "De goblin heeft je op een pijnlijke manier vermoord <br> <b>probeer het opnieuw!</b>"
 
     document.getElementById("button1").innerHTML = "ga terug "
@@ -188,6 +202,8 @@ function goblinterug(){
 // kisten // zwaard // kisten //
 
 function kisten(){
+	console.log("level 1 // kisten doorzoeken")
+
 if ( getzwaard == 'nee'){
 	document.getElementById("description").innerHTML = "je hebt een oud zwaard gevonden in een van de kisten"
     var zwaard = document.createElement('img');
@@ -201,6 +217,8 @@ if ( getzwaard == 'nee'){
       }
 }
 function deur(){
+	console.log("level 1 // de deur")
+
  if ( getsleutel == 'ja'){
  	 castle()
 
@@ -213,6 +231,7 @@ function deur(){
 // level 2 // blood and wine // level 2 //
 // level 2 // blood and wine // level 2 //
 function castle(){
+	console.log("level 2 // het kasteel")
 
     achtergrond = document.getElementById('game-container')
     achtergrond.style.backgroundImage = " url('img/castle.jpg') ";
@@ -229,7 +248,23 @@ function castle(){
     document.getElementById("button3").setAttribute('onclick',"kistenCastle()")
 
 }
+function onderzoekCastle(){
+	console.log("level 2 // de dwerg")
+
+	document.getElementById("description").innerHTML = 'Je komt een dwerg genaamt <b>Dennis van oakenshild tegen</b>'
+
+	achtergrond = document.getElementById('game-container')
+    achtergrond.style.backgroundImage = " url('img/castleDwarf.jpg') ";
+
+    document.getElementById("button1").innerHTML = "je verbeterd je zwaard <br> <b>(voor coins)<b>"
+    document.getElementById("button2").innerHTML = "je gaat terug <br> <b>( terug naar kasteel hall)</b>"
+    
+    document.getElementById('button3').style.display =' none'
+}
+
+
 function deurCastle(){
+	console.log("level 2 // de gladiator")
 
 	achtergrond = document.getElementById('game-container')
     achtergrond.style.backgroundImage = " url('img/castleguy.jpg') ";
@@ -244,6 +279,16 @@ function deurCastle(){
 
     document.getElementById("description").innerHTML = 'Je komt strijder <b> Ray van Evemor </b> tegen'
 
+}
+function aanvalGuy(){
+
+	document.getElementById("description").innerHTML = 'Je zwaard is gebroken door de machtige <b>Ray</b>'
+
+	zwaard.src = "img/zwaardkapot.png"
+}
+function terugGuy(){
+
+	castle()
 }
 
 
